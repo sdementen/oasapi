868e4ff6bc038eb49b0713cd5e9b54e3f4d550bc
GFJ138 <sebastien.dementen@engie.com>
2020-01-08 19:08:26 +0100

    Bump version: 0.1.1 → 0.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ project = 'The Open API Specifications Advanced Python Introspection library'
 year = '2019'
 author = 'Sebastien de Menten'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.1'
+version = release = '0.1.2'
 
 pygments_style = 'trac'
 templates_path = ['.']
