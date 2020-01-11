fae2a9493a9e7b9d0b3e820aec19af0ba6d8718a
GFJ138 <sebastien.dementen@engie.com>
2020-01-11 15:00:08 +0100

    fix coverage of __main__ with pragma directive

--- src/oasapi/__main__.py
+++ src/oasapi/__main__.py
@@ -8,7 +8,8 @@ Why does this file exist, and why __main__? For more info, read:
 - https://docs.python.org/2/using/cmdline.html#cmdoption-m
 - https://docs.python.org/3/using/cmdline.html#cmdoption-m
 """
-from oasapi.cli import main
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
+    from oasapi.cli import main
+
     main()
